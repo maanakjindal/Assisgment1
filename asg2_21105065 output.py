4ac52ebf2e3f47e5fb8675ca1ff4070c569f5e5b
maanakjindal <96733609+maanakjindal@users.noreply.github.com>
2022-01-22 17:43:22 +0530

    Add files via upload

---
+++ asg2_21105065 output.py
@@ -0,0 +1,48 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\dell\OneDrive\Desktop\que 1.py ===============
+the solution of question 1 is
+(a)	 the length of the input string is :35
+(b)	 the string in the reverse order is:egaugnal evitisnes esac a si nohtyP
+(c)	 the new string is:a case sensitive
+(d)	 the replaced string is : Python is object oriented language
+(e)	 the index of substring a in input string is :10
+(f)	 the input string without white spaces :Pythonisacasesensitivelanguage
+
+=============== RESTART: C:\Users\dell\OneDrive\Desktop\que 2.py ===============
+the solution of question 2 is
+enter your name:maanak jindal 
+enter your SID:21105065
+enter your departnment name:ECE
+enter your cgpa:9.9
+HEY,maanak jindal  Here!
+ MY SID is 21105065
+ I am from ECE and my CGPA is 9.900000
+
+=============== RESTART: C:\Users\dell\OneDrive\Desktop\que 3.py ===============
+the solution of question 3 is
+(a)	 a&b is : 8
+(b)	 a|b is : 58
+(c)	 a^b is : 50
+(d)	 left shift both a and b with 2 bits:a=224,b=40
+(e)	 right shift a with 2 bits and b with 4 bits:a=14,b=0
+
+=============== RESTART: C:\Users\dell\OneDrive\Desktop\que 4.py ===============
+the solution of question 4 is
+enter first number:34
+enter second number:44
+enter third number:22
+the largest number is  44
+
+=============== RESTART: C:\Users\dell\OneDrive\Desktop\que 5.py ===============
+the solution of question 5 is
+enter the string-hello my name is maanak
+yes
+
+==================================================== RESTART: C:\Users\dell\OneDrive\Desktop\que 6.py ===================================================
+the solution of question 6 is
+enter the first side of triangle-23
+enter the second side of the triangle-44
+enter the third side of the triangle-32
+yes.. the given input sides can form a triangle
